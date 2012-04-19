678ae8375621eceed267fdfe826e37b1a1687a98
unknown <eschuchardt@evin.(none)>
2012-04-19 17:02:17 -0500

    Demo - show start screen and show card outlines

--- src/schuchardt/evin/poker/MainActivity.java
+++ src/schuchardt/evin/poker/MainActivity.java
@@ -3,13 +3,20 @@ package schuchardt.evin.poker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	//Debug
+	private final static boolean D = true;
+	private final static String TAG = "MainActivity";
+		
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	if(D) Log.d(TAG, "in onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
@@ -18,6 +25,7 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
+				if(D) Log.d(TAG, "in onClick");
 				Intent i = new Intent(MainActivity.this, Poker2Activity.class);
 				startActivity(i);
 			}
--- src/schuchardt/evin/poker/Poker2Activity.java
+++ src/schuchardt/evin/poker/Poker2Activity.java
@@ -1,13 +1,36 @@
 package schuchardt.evin.poker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Poker2Activity extends Activity {
+	
+	//Debug
+	private final static boolean D = true;
+	private final static String TAG = "Poker2Activity";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	if(D) Log.d(TAG, "in onCreate");
         super.onCreate(savedInstanceState);
+        if(D) Log.d(TAG, "success super.onCreate(savedInstanceState);");
         setContentView(R.layout.play_view);
+        if(D) Log.d(TAG, "success setContentView(R.layout.play_view);");
+        
+        
+        ImageView card1 = (ImageView)findViewById(R.id.card1);
+        card1.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Poker2Activity.this, MainActivity.class);
+				startActivity(i);
+			}
+		});
     }
 }
